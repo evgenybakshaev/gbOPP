07ee33c535abc21982d1a6aed026d1d73cf43416
Evgeny Bakshaev <bakshaev@list.ru>
2020-05-24 11:16:02 +0500

    Deleted Lessons on branch

--- Lesson3/task2/src/pasengercar.cpp
+++
@@ -1,7 +0,0 @@
-#include "pasengercar.h"
-#include <iostream>
-PasengerCar::PasengerCar(string company)
-  : Car(company, "PasengerCar")
-{
-  std::cout << "PassangerCar(" << company << ")" << std::endl;
-}
