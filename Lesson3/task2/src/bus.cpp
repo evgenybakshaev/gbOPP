1a3f2d6531aeab7dc3f632776b47c7cd4b44090d
Евгений Бакшаев <bei@live.ru>
2020-05-28 13:50:11 +0500

    Deleted Lessons folders from blackjack branch

--- Lesson3/task2/src/bus.cpp
+++
@@ -1,7 +0,0 @@
-#include "bus.h"
-#include <iostream>
-Bus::Bus(string company)
-  : Car(company, "Bus")
-{
-  std::cout << "Bus(" << company << ")" << std::endl;
-}
--- Lesson3/task2/src/minivan.cpp
+++
@@ -1,7 +0,0 @@
-#include "minivan.h"
-#include <iostream>
-Minivan::Minivan(string company)
-  : Car(company, "Minivan"), PasengerCar(company), Bus(company)
-{
-  std::cout << "Minivan(" << company << ")" << std::endl;
-}
